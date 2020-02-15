73d788df84cb8a6a839fe80c6777fe1b3728cf36
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2020-02-15 11:04:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,26 @@
 import requests
 import json
 
-ENDPOINT_URL = "https://api.radar.io/v1/users"
+USERS_ENDPOINT = "https://api.radar.io/v1/users"
+GEOFENCES_ENDPOINT = "https://api.radar.io/v1/geofences"
+TEST_SECRET_KEY = "prj_test_sk_a6a5b26c2ebab809f5d0789d42b5bae913982def"
 TEST_PUBLISHABLE_KEY = "prj_test_pk_0ae22efb1fa81ae4af6c3e91bd2f151931689ff8"
 
-response = requests.get(url=ENDPOINT_URL)
+request_headers = {
+    "Authentication": TEST_SECRET_KEY
+}
+
+test_input = {
+    "description": "test geofence",
+    "type": "circle",
+    "coordinates": [-79.18829441070557, 43.78636197917316],
+    "radius": 50,
+    "tag": "safety",
+    "enabled": "true",
+}
+
+
+response = requests.post(url=GEOFENCES_ENDPOINT, data=json.dumps(test_input), headers=request_headers)
 response_data = response.json()
 
 print(response_data)
-print("hello world")
-2 + 5
