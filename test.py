70adbe71e88b9f0753d97d011f1fec93dc78d780
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2020-02-15 15:20:24 -0500

    Updated test.py, created test.java

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import json
 
 USERS_ENDPOINT = "https://api.radar.io/v1/users"
 GEOFENCES_ENDPOINT = "https://api.radar.io/v1/geofences"
+EVENTS_ENDPOINT = "https://api.radar.io/v1/events"
 TEST_SECRET_KEY = "prj_test_sk_a6a5b26c2ebab809f5d0789d42b5bae913982def"
 TEST_PUBLISHABLE_KEY = "prj_test_pk_0ae22efb1fa81ae4af6c3e91bd2f151931689ff8"
 
@@ -10,7 +11,9 @@ request_headers = {
     "Authorization": TEST_SECRET_KEY
 }
 
-test_input = {
+''' above code is useful for actual program '''
+
+test_geofence_input = {
     "description": "test geofence",
     "type": "circle",
     "coordinates": [-79.18829441070557, 43.78636197917316],
@@ -20,7 +23,12 @@ test_input = {
 }
 
 
-response = requests.post(url=GEOFENCES_ENDPOINT, data=test_input, headers=request_headers)
-response_data = response.json()
+geofence_response = requests.post(url=GEOFENCES_ENDPOINT, data=test_geofence_input, headers=request_headers)
+geofence_response_data = geofence_response.json()
 
-print(response_data)
+user_response = requests.get(url=USERS_ENDPOINT, headers=request_headers)
+user_response_data = user_response.json()
+
+
+print(geofence_response_data)
+print(user_response_data)
