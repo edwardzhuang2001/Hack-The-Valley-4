0a93200147b2074a09141fede4adec9333e2a4ec
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2020-02-15 11:16:45 -0500

    Created a geofence in test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ TEST_SECRET_KEY = "prj_test_sk_a6a5b26c2ebab809f5d0789d42b5bae913982def"
 TEST_PUBLISHABLE_KEY = "prj_test_pk_0ae22efb1fa81ae4af6c3e91bd2f151931689ff8"
 
 request_headers = {
-    "Authentication": TEST_SECRET_KEY
+    "Authorization": TEST_SECRET_KEY
 }
 
 test_input = {
@@ -20,7 +20,7 @@ test_input = {
 }
 
 
-response = requests.post(url=GEOFENCES_ENDPOINT, data=json.dumps(test_input), headers=request_headers)
+response = requests.post(url=GEOFENCES_ENDPOINT, data=test_input, headers=request_headers)
 response_data = response.json()
 
 print(response_data)
