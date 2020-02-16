d3dcb927f6156d4a19c2175794c03d84b0e82c15
Umang-Rajkarnikar <59628796+Umang-Rajkarnikar@users.noreply.github.com>
2020-02-16 02:42:19 -0500

    Update MapsActivity.java

--- CommunityWatchApp/app/src/main/java/com/example/communitywatchapp/MapsActivity.java
+++ CommunityWatchApp/app/src/main/java/com/example/communitywatchapp/MapsActivity.java
@@ -17,7 +17,6 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
