d3d5b052998b5804f7e41dd71cceeca65bcf5a74
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2020-02-16 04:00:22 -0500

    Update maven stuff

--- my-app/src/main/java/org/json/app/App.java
+++ my-app/src/main/java/org/json/app/App.java
@@ -1,7 +1,7 @@
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class ParseJSON
+public class App
 {
     static String json = "{'pageInfo':{'pageName':'Homepage','logo':'https://www.example.com/logo.jpg'},'posts':[{'post_id':'0123456789','actor_id':'1001','author_name':'Jane Doe','post_title':'How to parse JSON in Java','comments':[],'time_of_post':'1234567890'}]}";
 
