4c05ec7d8f73bdd6dbebf1a963ab0f2962bbae5d
Umang-Rajkarnikar <59628796+Umang-Rajkarnikar@users.noreply.github.com>
2020-02-16 03:19:38 -0500

    Added notifications, report, and emergency message pages.

--- CommunityWatchApp/app/src/main/java/com/example/communitywatchapp/Menu.java
+++ CommunityWatchApp/app/src/main/java/com/example/communitywatchapp/Menu.java
@@ -2,7 +2,9 @@ package com.example.communitywatchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void clickedNotif(View view){
+        if(view.getId()==R.id.btnNotifications){
+            Intent intent = new Intent(this, Notifications.class);
+            startActivity(intent);
+        }
+    }
+
+    public void clickedReport(View view){
+        if(view.getId()==R.id.btnReport){
+            Intent intent = new Intent(this, Report.class);
+            startActivity(intent);
+        }
+    }
+
+    public void clickedEmergency(View view){
+        if(view.getId()==R.id.btnEmergency){
+            Intent intent = new Intent(this, EmergencyMessage.class);
+            startActivity(intent);
+        }
     }
 }
